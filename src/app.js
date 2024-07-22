44167391af16c9a79eb2889c1bee7335ddd5c59a
Daria <149780226+dariaztva10@users.noreply.github.com>
2024-07-22 11:00:41 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Variable para los palos de la baraja
+  const palos = ["heart", "spade", "diamond", "club"];
+  const simbolos = ["♠", "♣", "♥", "♦"];
+  //Variable para el número de la carta
+  const cartas = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  //Generar un palo aleatorio
+  let palosAleatorios = Math.floor(Math.random() * palos.length);
+  //Generar cartas aleatorios
+  let cartasAleatorias = Math.floor(Math.random() * cartas.length);
+
+  //Quiero obtener un palo accediendo al indice de palos aleatorios
+  let paloObtenido = palos[palosAleatorios];
+
+  //Quiero obtener una carta accediendo al indice de cartas aleatorios
+  let cartaObtenida = cartas[cartasAleatorias];
+
+  //Vamos a asignar a una variable, el elemento obtenido del DOM desde el div
+  let cartaDiv = document.getElementById("card");
+  //PARA
+  console.log();
 };
