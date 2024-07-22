a74220b9d6949383f8c75c7573491cccbbc578d3
Daria <149780226+dariaztva10@users.noreply.github.com>
2024-07-22 21:47:08 +0000

    finalizando el proyecto

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import "bootstrap";
 import "./style.css";
 
 import "./assets/img/4geeks.ico";
-/*  
+
 window.onload = function() {
   //Variable para los palos de la baraja
   const palos = ["heart", "spade", "diamond", "club"];
@@ -24,20 +24,31 @@ window.onload = function() {
     "A"
   ];
 
-  //Generar un palo aleatorio
+  //Generar un índice aleatorio para el palo y la carta
   let palosAleatorios = Math.floor(Math.random() * palos.length);
-  //Generar cartas aleatorios
   let cartasAleatorias = Math.floor(Math.random() * cartas.length);
 
-  //Quiero obtener un palo accediendo al indice de palos aleatorios
+  //Quiero obtener un palo y un símbolo coincidente
   let paloObtenido = palos[palosAleatorios];
-
-  //Quiero obtener una carta accediendo al indice de cartas aleatorios
   let cartaObtenida = cartas[cartasAleatorias];
+  let simboloObtenido = simbolos[palosAleatorios];
 
-  //Vamos a asignar a una variable, el elemento obtenido del DOM desde el div
+  //Vamos a asignar a una variable el elemento obtenido del DOM desde el div
   let cartaDiv = document.getElementById("card");
-  //PARA
-  console.log();
+  //Añadir la clase al palo seleccionado
+  cartaDiv.classList.add(paloObtenido);
+
+  //Obtener los elementos del DOM donde se mostrarán los símbolos y números
+  let simboloTopDiv = cartaDiv.querySelector(".top-left.symbol");
+  let simboloBottomDiv = cartaDiv.querySelector(".bottom-right.symbol");
+  let numeroDiv = cartaDiv.querySelector(".center.number");
+
+  // Actualizar los contenidos de los elementos del DOM
+  simboloTopDiv.textContent = simboloObtenido;
+  simboloBottomDiv.textContent = simboloObtenido;
+  numeroDiv.textContent = cartaObtenida;
+
+  console.log(
+    `Palo obtenido: ${paloObtenido}, Carta obtenida: ${cartaObtenida}, Símbolo obtenido: ${simboloObtenido}`
+  );
 };
-*/
